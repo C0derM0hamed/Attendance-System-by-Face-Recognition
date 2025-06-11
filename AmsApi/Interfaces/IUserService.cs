20020cb7a406651873c13a1a246cf7f364ce4cb3
C0derM0hamed <mohamedmostafa2011360@gmail.com>
2025-06-11 14:46:17 +0300

    Squashed 'backend/' content from commit 40017f5
    git-subtree-dir: backend
    git-subtree-split: 40017f50503514263b27721167f16d37745bd222

---
+++ AmsApi/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace AmsApi.Interfaces
+{
+    public interface IUserService
+    {
+        Task<RegisterResponse> RegisterUserAsync(CreateUserDto dto);
+        Task<AuthResponse> LoginAsync(LoginDto dto);
+    }
+}
